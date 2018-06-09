33ebb8d0e9ba42df008cdfac7f9e34f9a76c8104
Shashank Shekhar <shashank7200@gmail.com>
2018-06-09 16:55:24 +0530

    bootstrapped the appication

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from 'material-ui-search-bar'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <SearchBar
+      onChange={() => console.log('onChange')}
+      onRequestSearch={() => console.log('onRequestSearch')}
+      style={{
+        margin: '0 auto',
+        maxWidth: '90%'
+      }}
+    />
+      </div>
+    );
+  }
+}
+
+export default App;
