1158e79b21bf04800b52e861f6617542c24b8431
Shashank Shekhar <shashank7200@gmail.com>
2018-06-09 17:01:37 +0530

    decalred the contrcutor for the app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from 'material-ui-search-bar'
+import SearchBar from 'material-ui-search-bar';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: null
+    }
+  }
+
   render() {
     return (
       <div className="App">
